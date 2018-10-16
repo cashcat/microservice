abdd3053afebb73f9697dc8c69832a8bf7699e5f
cashcat <cashcat@gmail.com>
2018-10-16 23:22:52 +0800

    add repo

---
+++ src/main/java/lcw/dao/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package lcw.dao;
+
+import lcw.model.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
